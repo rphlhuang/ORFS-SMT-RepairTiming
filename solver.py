c374d6e34f308064de13e81d8183eba1206e8f70
Chad Baker <chadster5200@gmail.com>
2025-11-17 12:25:33 -0800

    changed " to '

--- solver.py
+++ solver.py
@@ -22,22 +22,22 @@ data = {
 }
 
 buffer_sizes = set()
-for i in data["path_data"]["buffer_slots"]:
-    for cell in i["choices"]:
-        buffer_sizes.add(cell["cell_type"])
+for i in data['path_data']['buffer_slots']:
+    for cell in i['choices']:
+        buffer_sizes.add(cell['cell_type'])
 buffer_sizes = list(buffer_sizes)
 print("Buffer sizes: " + str(buffer_sizes))
 
 slot_ids = set()
-for i in data["path_data"]["buffer_slots"]:
-        slot_ids.add(i["slot_id"])
+for i in data['path_data']['buffer_slots']:
+        slot_ids.add(i['slot_id'])
 slot_ids = list(slot_ids)
 print("Slot ids: " + str(slot_ids))
 
 s = Solver()
 
 # Construct boolean decision vars
-decision_vars = [Bool(f"S_{slot["slot_id"]}_{cell}") for slot in data["path_data"]["buffer_slots"] for cell in buffer_sizes]
+decision_vars = [Bool(f"S_{slot['slot_id']}_{cell}") for slot in data['path_data']['buffer_slots'] for cell in buffer_sizes]
 print("Decision vars: ", str(decision_vars))
 
 # One-hot constraint: at least one cell per slot, but no more
