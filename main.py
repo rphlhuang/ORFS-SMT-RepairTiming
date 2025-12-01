91031945862f691556eb4ed2563fbb6d27f6a7eb
Chad Baker <chadster5200@gmail.com>
2025-12-01 15:21:25 -0800

    setup fake slack

--- main.py
+++ main.py
@@ -5,14 +5,19 @@ from solver import *
 #   1. Start sta
 #   2. Start thread
 #   3. Run setup_sta.tcl
+print("Setting up STAcontroller")
 sta = STAController("setup_sta.tcl")
 
 # Creating SMT instance
+print("Setting up SMT solver")
 SMT_inst = SMTsolver(data)
 
+setup_slack = -3
+hold_slack = -3
 # Runs SMT
 while True:
     # Outputs chosen buffer sizes to buffer.sol
+    print("Running solve() on SMT_inst")
     SAT = SMT_inst.solve()
 
     # If SAT 
@@ -23,12 +28,17 @@ while True:
         # Grabs values from buffer.sol
         # runs apply_buffer_solution
         # then runs timing slack with new buffers
-        setup_slack, hold_slack = sta.apply_and_get_slacks()
+        print("running sta.apply_and_get_slack")
+        setup_slack += 1
+        hold_slack += 1
+        # setup_slack, hold_slack = sta.apply_and_get_slacks()
         
         # Slack is still negative
+        print("Checking if slack is negative")
         if (setup_slack <= 0) or (hold_slack <= 0):
             # add conflict clause
-            SMT_inst.add_conflict()
+            print("Adding conflict clause")
+            SMT_inst.add_conflict(SMT_inst.model)
             print("STA output has negative slack, adding conflict clause...")
         
         # Slack is now positive
