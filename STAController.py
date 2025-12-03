0d97662a3be592e7e3cc23d4be349bfe6e3aba90
Chad Baker <chadster5200@gmail.com>
2025-12-03 09:19:56 -0800

    Added print statements

--- STAController.py
+++ STAController.py
@@ -8,6 +8,7 @@ OpenROAD = "../../tools/install/OpenROAD/bin/openroad"
 class STAController:
     def __init__(self, setup_file="setup_sta.tcl"):
         # Start OpenSTA as a persistent background process
+        print("Starting STA subprocess")
         self.proc = subprocess.Popen(
             [OpenSTA, "-no_init"],
             stdin=subprocess.PIPE,
@@ -28,16 +29,20 @@ class STAController:
 
     # Creates a background thread that reads OpenSTA output
     def _reader_thread(self):
+        print("reader thread")
         for line in self.proc.stdout:
+            print("[STA]", line)     
             self.output_queue.put(line.rstrip("\n"))
 
     # Sends a command to OpenSTA
     def send_cmd(self, cmd):
+        print(f"sending cmd: {cmd}")
         self.proc.stdin.write(cmd + "\n")
         self.proc.stdin.flush()
 
     # Returns a list of all lines with markers
     def read_until(self, markers):
+        print("reading util")
         lines = []
         while True:
             line = self.output_queue.get()
@@ -48,27 +53,34 @@ class STAController:
 
     # Applies new buffer choices and returns setup and hold slack
     def apply_and_get_slacks(self, solfile="buffers.sol"):
+        print("applying buffers")
         # Apply buffers
         self.send_cmd(f"apply_buffer_solution {solfile}")
 
         # Compute timing
+        print("computing worst slack")
         self.send_cmd("compute_worst_slacks")
 
-        # Wait until we get WS_SETUP and WS_HOLD
-        lines = self.read_until(["WS_HOLD"])
+        # Wait until we see the 'worst slack min' line (hold)
+        # We expect both 'worst slack max' and 'worst slack min' to appear in this block.
+        lines = self.read_until(["worst slack min"])
 
         setup_slack = None
         hold_slack = None
 
         for line in lines:
-            if line.startswith("WS_SETUP"):
-                setup_slack = float(line.split()[1])
-            if line.startswith("WS_HOLD"):
-                hold_slack = float(line.split()[1])
+            if line.startswith("worst slack max"):
+                # e.g. "worst slack max -0.10"
+                setup_slack = float(line.split()[-1])
+            elif line.startswith("worst slack min"):
+                # e.g. "worst slack min 0.47"
+                hold_slack = float(line.split()[-1])
 
         return setup_slack, hold_slack
 
+
     # Closes OpenSTA Thread
     def close(self):
+        print("closing thread")
         self.send_cmd("exit")
         self.proc.wait()
