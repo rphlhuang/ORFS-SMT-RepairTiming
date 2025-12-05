4bfc35d5fd8a4dde9d6e92ab8a9d1d67ec242d60
Chad Baker <chadster5200@gmail.com>
2025-12-05 12:19:55 -0800

    Many changes: - no longer using STAcontroller, setup_sta.tcl, and while loop - running openROAD subprocess in main.py

--- main.py
+++ main.py
@@ -1,16 +1,10 @@
-from STAController import *
 from solver import *
+import subprocess
 import json
 
-THRESHOLD = 0.0
+OpenROAD = "../../tools/install/OpenROAD/bin/openroad"
 
-# This will
-#   1. Start sta
-#   2. Start thread
-#   3. Run setup_sta.tcl
-print("Setting up STAcontroller")
-sta = STAController("setup_sta.tcl")
-
+# grab data values
 with open("solver_input.json", "r") as f:
     data = json.load(f)
 
@@ -18,42 +12,23 @@ with open("solver_input.json", "r") as f:
 print("Setting up SMT solver")
 SMT_inst = SMTsolver(data)
 
-# Runs SMT
-iter = 1
-while True:
-    # Outputs chosen buffer sizes to buffer.sol
-    print("-------------------------")
-    print(f"[ITER] {iter}")
-    print("-------------------------")
-    print("Running solve() on SMT_inst")
-    SAT = SMT_inst.solve()
+# Outputs chosen buffer sizes to buffer.sol
+print("Running solve() on SMT_inst")
+SAT = SMT_inst.solve()
 
-    iter += 1
-    # If SAT 
-    #   run apply_buffers
-    # else
-    #   No valid solution
-    if SAT:
-        # Grabs values from buffer.sol
-        # runs apply_buffer_solution
-        # then runs timing slack with new buffers
-        print("running sta.apply_and_get_slack")
-        setup_slack, hold_slack = sta.apply_and_get_slacks()
-        
-        # add conflict clause
-        # print("STA output has negative slack, adding conflict clause...")
-        # SMT_inst.add_conflict(SMT_inst.model)
-        
-        # Slack is now positive
-        # else:
-        print("STA output has optimal slack")
-        
-        # create new odb file
-        sta.send_cmd("write_current_db ../results/sky130hd/gcd/base/after_smt.odb\n")
-        break
-        
-    else:
-        print("Unsatisfiable. No buffer combination meets timing.")
-        break
-
-sta.close()
+# If SAT 
+#   run apply_buffers
+# else
+#   No valid solution
+if SAT:
+    # Grabs values from buffer.sol
+    # runs apply_buffer_solution
+    # then runs timing slack with new buffers
+    # creates new odb file
+    print("STA output has optimal slack")
+    subprocess.run(
+        [OpenROAD, "-exit", "apply_smt_buffers.tcl"],
+        check=True
+    )
+else:
+    print("Unsatisfiable. No buffer combination meets timing.")
